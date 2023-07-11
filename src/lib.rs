56d1d1f0efc31dfecdb644ec369cde6adde555dd
haruki7049 <tontonkirikiri@gmail.com>
2023-07-11 19:25:32 +0900

    added rational and irrational

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod real_number {
+    /// this number can show fraction number
+    /// TODO: to express any number as a fraction
+    pub mod rational {
+        use fraction_rs::Fraction;
+
+    }
+
+    /// this number cannot show fraction number
+    /// TODO: create square root format and other irrational number
+    pub mod irrational {
+        const SQUARE_ROOT2 = ;
+    }
+}
